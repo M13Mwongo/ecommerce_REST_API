52a37e9c340e05fe567931ed41ea78b2d044ef8b
Mwiti Mwongo <62349438+M13Mwongo@users.noreply.github.com>
2023-08-31 15:07:13 +0300

    updating dependencies and requirements

--- eshop/settings.py
+++ eshop/settings.py
@@ -28,7 +28,7 @@ if IS_HEROKU_APP:
     ALLOWED_HOSTS = ["*"]
 else:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                     'eshop-env.eba-2ddtafk9.us-west-2.elasticbeanstalk.com']
+                     'eshop-API-env.eba-f3r329z2.us-west-2.elasticbeanstalk.com']
 
 
 # Rest framework settings
